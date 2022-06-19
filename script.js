e10da859a3569bb649f091e4d5f0c7d674fc37e2
Devanshi1901 <106826385+Devanshi1901@users.noreply.github.com>
2022-06-19 13:42:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+a = document.getElementById("submit")
+a.addEventListener("click", displaydata);
+
+function displaydata() {
+    const x = document.forms['myform'];
+    let text = "";
+    for (let i = 0; i < x.length; i++) {
+        text += x.elements[i].value + "<br>";
+    }
+    document.getElementById("login").innerHTML = text;
+    console.log(text);
+}
